1e31ed580ed56a88d4582c338152f1e6cac135fc
JoseGutierrez02 <josearmandog02@gmail.com>
2021-06-02 17:19:01 -0400

    error handler middleware was added

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ const express = require('express');
 const app = express();
 const { config } = require('./config');
 const moviesApi = require('./routes/movies');
+const { logErrors, errorHandler } = require('./utils/middleware/errorHandler');
 
 // body-parser middleware
 app.use(express.json());
 
 // routes
 moviesApi(app);
+
+// error-handling middleware
+app.use(logErrors);
+app.use(errorHandler);
 
 app.listen(config.port, () => {
   console.log(`Server listening at http://localhost:${config.port}`);
